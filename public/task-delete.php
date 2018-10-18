7805d8107b2e3f9899637aeed70280a99d717164
平石匡 <masahiraishi22@gmail.com>
2018-10-18 22:41:22 +0900

    delete処理

---
+++ public/task-delete.php
@@ -0,0 +1,17 @@
+<?php
+require_once '../app.php';
+require_once join_paths([MODELS_ROOT,'Task.php']);
+
+$session = session('tasks');
+
+if (!$session->veryfyCsrfToken()){
+    $session->set('errors',['セッションが切れました。ページをリロードしてください!']);
+    redirect('/');
+}
+
+
+$id = request_get('id');
+
+Task::delete($id);
+
+redirect('/');
